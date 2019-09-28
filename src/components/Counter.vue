b558b003937aec955dbb4a95f844016b29bb7612
kkritii <kritiipraz@gmail.com>
2019-09-28 13:12:02 +0545

    hello

---
+++ src/components/Counter.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="heading">
+        <h4 class="title">Increment or decrement a number</h4>
+      </div>
+      <div class="content">
+        <button @click="increment" type="button" class="btn">+</button>
+        <span class="counter">{{counter}}</span>
+        <button @click="decrement" type="button" class="btn">-</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
+  }
+};
+</script>
+
+
+<style scoped lang="scss">
+.container {
+  margin: 0 auto;
+  max-width: 768px;
+}
+.title {
+  font-size: 18px;
+  font-weight: 600;
+  margin-bottom: 18px;
+}
+.card {
+  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
+  padding: 24px;
+}
+
+.btn {
+  color: #fff;
+  cursor: pointer;
+  background-color: #007bff;
+  border-color: #007bff;
+  padding: 6px 12px;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 6px;
+  transition: all 0.1s ease-in;
+
+  &:hover {
+    background-color: #0069d9;
+    border-color: #0062cc;
+  }
+}
+
+.counter {
+  margin: 0 12px;
+  font-size: 18px;
+  font-weight: 600;
+}
+</style>
